f7820e16f292e9bddfdbb0d00471cdf2a4b6d46e
Jason Costomiris <jcostom@jasons.org>
2016-05-12 22:34:06 -0400

    Added BGP Neighbor tool

---
+++ bgp-neighbor/bgp-neighbor.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+# Based (heavily) on the work of @macrujugl
+# https://github.com/macrujugl/pyez
+
+from jnpr.junos.factory import loadyaml
+from jnpr.junos import Device
+
+host = 'srx1'
+user = 'autobot'
+password = 'juniper123'
+yamlfile = 'bgp_table.yml'
+
+def main():
+    globals().update( loadyaml(yamlfile))
+    dev = Device(host=host, user=user, password=password)
+    dev.open()
+    peerTable = bgpNeighbor(dev)
+    peerTable.get()
+    for peer in peerTable:
+        print peer.items()
+
+    dev.close()
+
+if __name__ == "__main__":
+    main()
