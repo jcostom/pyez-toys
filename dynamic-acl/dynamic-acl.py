089c70af02f943b2652e12d3ba57293051979d94
Jason Costomiris <jcostom@jasons.org>
2018-06-05 17:30:53 -0400

    Updated to Python 3

--- dynamic-acl/dynamic-acl.py
+++ dynamic-acl/dynamic-acl.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from jnpr.junos import Device
 from jnpr.junos.utils.config import Config
@@ -20,21 +20,21 @@ def main():
         dev = Device(host=host, user=user, password=password)
         # Open the connection & config
         try:
-            print "Opening connnection to:", host
+            print("Opening connnection to: {}".format(host))
             dev.open()
         except Exception as err:
-            print "Cannot connect to device:", err
+            print("Cannot connect to device: {}".format(err))
             return
         dev.bind(cu=Config)
         # Lock the configuration, load changes, commit
-        print "Locking the configuration on:", host
+        print("Locking the configuration on: {}".format(host))
         try:
             dev.cu.lock()
         except LockError:
-            print "Error: Unable to lock configuration on:", host
+            print("Error: Unable to lock configuration on: {}".format(host))
             dev.close()
             return
-        print "Loading configuration changes on:", host
+        print("Loading configuration changes on: {}".format(host))
         try:
             set_commands = """
             delete policy-options prefix-list edge-block
@@ -44,37 +44,37 @@ def main():
             dev.cu.load(template_path=template,
                         template_vars=config, format='text')
         except ValueError as err:
-            print err.message
+            print(err.message)
         except Exception as err:
             if err.rsp.find('.//ok') is None:
                 rpc_msg = err.rsp.findtext('.//error-message')
-                print "Unable to load config changes: ", rpc_msg
-            print "Unlocking the configuration on:", host
+                print("Unable to load config changes: {}".format(rpc_msg))
+            print("Unlocking the configuration on: {}".format(host))
             try:
                 dev.cu.unlock()
             except UnlockError:
-                print "Error: Unable to unlock configuration on:", host
+                print("Error: Unable to unlock config on: {}".format(host))
             dev.close()
             return
-        print "Committing the configuration on:", host
+        print("Committing the configuration on: {}".format(host))
         try:
             dev.cu.commit()
-            # print dev.cu.diff()
+            # print(dev.cu.diff())
         except CommitError:
-            print "Error: Unable to commit configuration on:", host
-            print "Unlocking the configuration on:", host
+            print("Error: Unable to commit configuration on: {}".format(host))
+            print("Unlocking the configuration on: {}".format(host))
             try:
                 dev.cu.unlock()
             except UnlockError:
-                print "Error: Unable to unlock configuration on:", host
+                print("Error: Unable to unlock config on: {}".format(host))
             dev.close()
             return
         print "Unlocking the configuration on:", host
         try:
             dev.cu.unlock()
         except UnlockError:
-            print "Error: Unable to unlock configuration on:", host
-        print "Closing connection to:", host
+            print("Error: Unable to unlock config on: {}".format(host))
+        print("Closing connection to: {}".format(host))
         dev.close()
 
 
