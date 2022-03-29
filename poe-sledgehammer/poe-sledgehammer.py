14a5eeb21dca43537d31010a6e69e372739cfda2
Jason Costomiris <jcostom@jasons.org>
2022-03-29 12:36:10 -0400

    Updated port bouncer, added PoE sledgehammer

--- poe-sledgehammer/poe-sledgehammer.py
+++ poe-sledgehammer/poe-sledgehammer.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 
-# You ever need to bounce a port on a switch, but don't
-# want to, or just plain can't walk up to the switch
-# and pull a cable out? Don't feel like doing the
-# disable/commit/enable/commit dance manually?
-# This does the work for you.
-# Yes, you could also drop to a root shell and
-# use ifconfig, but JTAC might give you stern looks
-# for doing that.
+# This tool is a blunt instrument - HANDLE WITH CARE.
+# It disables PoE on all ports, commits the configuration,
+# then executes a rollback 1 and commits again.
+# You would only use this tool if you wanted to completely
+# disable PoE then rollback that change.
 
 import argparse
 import os
@@ -15,11 +12,10 @@ from jnpr.junos import Device
 from jnpr.junos.utils.config import Config
 
 parser = argparse.ArgumentParser(
-    description='Juniper Switch Port Bounce Utility'
+    description='Juniper Switch PoE Bounce Utility'
 )
 
 parser.add_argument('--switch', action="store")
-parser.add_argument('--port', action="store")
 parser.add_argument('--user', action="store",
                     default=os.getenv('USER'),
                     help="Will default to your current username.")
@@ -28,15 +24,9 @@ args = parser.parse_args()
 
 
 def main():
-    disableCommand = " ".join(
-        ["set interfaces", args.port, "disable"]
-    )
-    disableComment = " ".join(
-        ["shut port", args.port]
-    )
-    rollbackComment = " ".join(
-        ["rollback shut of port", args.port]
-    )
+    disableCommand = "set poe interface all disable"
+    disableComment = "drop the PoE sledgehammer on all ports"
+    rollbackComment = "rollback - restoring PoE"
 
     dev = Device(host=args.switch, user=args.user)
     print("Connecting to: {}".format(args.switch))
@@ -44,7 +34,7 @@ def main():
     dev.bind(cu=Config)
     print("Locking the configuration on: {}".format(args.switch))
     dev.cu.lock()
-    print("Now shutting port: {}".format(args.port))
+    print("Now shutting down PoE on all ports: {}".format(args.port))
     dev.cu.load(disableCommand, format='set')
     dev.cu.commit(comment=disableComment, timeout=180)
     print("Now executing rollback on: {}".format(args.switch))
