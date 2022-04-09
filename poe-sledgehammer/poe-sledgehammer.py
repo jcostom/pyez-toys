4bb3cbb90033e191f85ef400fa2af41d54ddcd7b
Jason Costomiris <jcostom@jasons.org>
2022-04-09 02:02:20 -0400

    change poe sledge to use logging instead of print()

--- poe-sledgehammer/poe-sledgehammer.py
+++ poe-sledgehammer/poe-sledgehammer.py
@@ -7,9 +7,19 @@
 # disable PoE then rollback that change.
 
 import argparse
+import logging
 import os
 from jnpr.junos import Device
 from jnpr.junos.utils.config import Config
+
+# Setup logger
+logger = logging.getLogger()
+ch = logging.StreamHandler()
+logger.setLevel(logging.INFO)
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter('[%(levelname)s] %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
 parser = argparse.ArgumentParser(
     description='Juniper Switch PoE Bounce Utility'
@@ -29,21 +39,21 @@ def main():
     rollbackComment = "rollback - restoring PoE"
 
     dev = Device(host=args.switch, user=args.user)
-    print("Connecting to: {}".format(args.switch))
+    logger.info("Connecting to: {}".format(args.switch))
     dev.open()
     dev.bind(cu=Config)
-    print("Locking the configuration on: {}".format(args.switch))
+    logger.info("Locking the configuration on: {}".format(args.switch))
     dev.cu.lock()
-    print("Now shutting down PoE on all ports: {}".format(args.port))
+    logger.info("Now shutting down PoE on all ports: {}".format(args.port))
     dev.cu.load(disableCommand, format='set')
     dev.cu.commit(comment=disableComment, timeout=180)
-    print("Now executing rollback on: {}".format(args.switch))
+    logger.info("Now executing rollback on: {}".format(args.switch))
     dev.cu.rollback(rb_id=1)
     dev.cu.commit(comment=rollbackComment, timeout=180)
-    print("Unlocking the configuration on: {}".format(args.switch))
+    logger.info("Unlocking the configuration on: {}".format(args.switch))
     dev.cu.unlock()
     dev.close()
-    print("Done!")
+    logger.info("Done!")
 
 
 if __name__ == "__main__":
